a6405a78e091c15cfa770a073f8b028d893f3f78
Wesley <wesleyorobosa73@gmail.com>
2025-04-18 03:06:23 +0100

    contact auth

--- auth.js
+++ auth.js
@@ -16,27 +16,56 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const booktable = document.querySelector("#booktable");
+const contact = document.querySelector("#sends");
 
-document.getElementById("booktable").addEventListener("click", async () => {
-  const name = document.getElementById("names").value;
-  const phone = document.getElementById("phoneno").value;
-  const date = document.getElementById("dates").value;
-  const time = document.getElementById("times").value;
-  const persons = document.getElementById("persons").value;
+if (booktable) {
+  booktable.addEventListener("click", async () => {
+    const name = document.getElementById("names").value;
+    const phone = document.getElementById("phoneno").value;
+    const date = document.getElementById("dates").value;
+    const time = document.getElementById("times").value;
+    const persons = document.getElementById("persons").value;
 
-  if (!name || !phone || !date || !time || !persons) {
-    alert("Please fill in all fields.");
-    return;
-  }
+    if (!name || !phone || !date || !time || !persons) {
+      alert("Please fill in all fields.");
+      return;
+    }
 
-  const data = { name, phone, date, time, persons };
+    const data = { name, phone, date, time, persons };
 
-  try {
-    const docRef = await addDoc(collection(db, "bookings"), data);
-    alert("Booking successful! ID: " + docRef.id);
-    window.location.reload();
-  } catch (err) {
-    console.error("Error saving booking:", err);
-    alert("Booking failed.");
-  }
-});
+    try {
+      const docRef = await addDoc(collection(db, "bookings"), data);
+      alert("Booking successful! ID: " + docRef.id);
+      window.location.reload();
+    } catch (err) {
+      console.error("Error saving booking:", err);
+      alert("Booking failed.");
+    }
+  });
+}
+
+if (contact) {
+  sends.addEventListener("click", async () => {
+    const name = document.getElementById("identity").value;
+    const email = document.getElementById("cont").value;
+    const subject = document.getElementById("subs").value;
+    const message = document.getElementById("mess").value;
+
+    if (!name || !email || !subject || !message) {
+      alert("Please fill in all fields");
+      return;
+    }
+
+    const userdata = { name, email, subject, message };
+
+    try {
+      const docRef = await addDoc(collection(db, "comments"), userdata);
+      alert("Message successfully submitted! ID: " + docRef.id);
+      window.location.reload();
+    } catch (err) {
+      console.log("error submitting message", err);
+      alert("submission failed");
+    }
+  });
+}
